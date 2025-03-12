0bc7c1b25890c561b593d6ab8a8c306ccc8dd671
UndercoverLegend <syedibrahim.adnan@elev.vittra.se>
2025-03-12 20:26:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ function filterGames(category) {
         }
     });
 }
+function loadGame(gameUrl) {
+    console.log("Loading game:", gameUrl); // Debugging log
+    window.location.href = gameUrl; // Opens the game in the current tab
+}
